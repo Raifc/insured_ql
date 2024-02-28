d4342d51206f2bc25ab77e239e6411604942356d
Raifc <raif.si@hotmail.com>
2024-02-28 16:53:50 -0300

    Adds cache

--- app/graphql/resolvers/policy_resolver.rb
+++ app/graphql/resolvers/policy_resolver.rb
@@ -8,11 +8,41 @@ class Resolvers::PolicyResolver < Resolvers::BaseResolver
 
   argument :id, ID, required: true
 
+  CACHE_EXPIRATION = 30.seconds
+
   def resolve(id:)
+    cached_policy = read_cached_policy(id)
+
+    if cached_policy
+      cached_policy
+    else
+      policy = fetch_policy(id)
+      cache_policy(id, policy) if policy
+      policy
+    end
+  end
+
+  private
+
+  def read_cached_policy(id)
+    Rails.cache.read("policy_#{id}")
+  end
+
+  def cache_policy(id, policy)
+    Rails.cache.write("policy_#{id}", policy, expires_in: CACHE_EXPIRATION)
+  end
+
+  def fetch_policy(id)
     response = Faraday.get("http://api_rest:3000/policies/#{id}")
 
-    return unless response.success?
+    return nil unless response.success?
 
     JSON.parse(response.body)
+  rescue Faraday::Error => e
+    puts "Error fetching policy: #{e.message}"
+    nil
+  rescue JSON::ParserError => e
+    puts "Error parsing JSON: #{e.message}"
+    nil
   end
 end
